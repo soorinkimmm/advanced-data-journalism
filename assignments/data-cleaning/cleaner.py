2d2bf8b24c55f9aabd6389cacc62a1e1eecf58c3
김수린 <sulin@mwc-109217.dhcp.missouri.edu>
2016-03-04 10:06:46 -0600

    Modify cleaner file

--- assignments/data-cleaning/cleaner.py
+++ assignments/data-cleaning/cleaner.py
@@ -1,1 +1,20 @@
 # Your code goes here
+import csv
+
+csvfile = open('./data/cleanme.csv', 'r')
+outfile = open('./data/clean.csv', 'w')
+
+reader = csv.DictReader(csvfile)
+writer = csv.DictWriter(outfile, reader.fieldnames)
+
+writer.writeheader()
+
+for row in reader:
+	row['first_name'] = row['first_name'].upper()
+	row['zip'] = row['zip'].zfill(5)
+	row['city'] = row['city'].replace.
+	writer.writerow(row)
+
+
+	
+
